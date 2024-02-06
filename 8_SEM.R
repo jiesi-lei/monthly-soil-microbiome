f2da6beb2b7bd5c61f47602f835278c09f9e387e
Jiesi Lei <43570023+jiesi-lei@users.noreply.github.com>
2024-02-06 15:40:52 -0600

    Update 8_SEM.R

--- 8_SEM.R
+++ 8_SEM.R
@@ -1,8 +1,13 @@
+# SEM for both Rh and Rs
+library(dplyr)
 library(lavaan)
 library(AICcmodavg)
 library(tidyverse)
 library(semPlot)
 library(vegan)
+
+source("0_Setup.R")
+source("0_Setup_gene.R")
 
 # source("lavaan.modavg.R")
 #group
@@ -28,6 +33,17 @@ summary(nmds1)
 nmds_plot <- nmds1
 func.struc = as.data.frame(scores(nmds1))[,1]*10
 func.richness = specnumber(gene)/20000
+
+#taxonomic profile: shannon index
+otu = read.delim("resample_zotutab_new_21567.txt",row.names = 1, header = T,check.names = TRUE, sep = "\t")
+otu = otu[-which(rowSums(otu)== 0),]
+otu = as.data.frame(t(otu))
+bactr.richness = specnumber(otu)/1000
+nmds1 <- metaMDS(log(otu+1), distance = 'bray', k = 4)
+nmds1
+summary(nmds1)
+nmds_plot <- nmds1
+bactr.struc = as.data.frame(scores(nmds1))[,1]*30
 
 # c.deg gene absolute abundance
 c.deg.select = c("amyA","cda","glucoamylase","nplT","pula",#Starch
@@ -74,108 +90,114 @@ semdat = data.frame(moisture, pH, temp, TN, TC, NH4N, NO3N, precipitation,
                     bactr.struc, bactr.richness,
                     copy_number, doy, warming)
 #write.table(semdat,"datasets/SEM/semdata.dat")
-semdat %>% {scale(.)} %>% {data.frame(.,warming)}-> semdat.scaled
+#semdat %>% {scale(.)} %>% {data.frame(.,warming)}-> semdat.scaled
 
-semdat.scaled.cool = semdat[id_cool,]
-semdat.scaled.warm = semdat[id_warm,]
+semdat.scaled.cool = scale(semdat[id_cool,])
+semdat.scaled.warm = scale(semdat[id_warm,])
+save(semdat.scaled.cool,semdat.scaled.warm,file = "SEM_RH/SEM_dat.RData")
 
-# write.csv(semdat.scaled.cool,"SEM/cool.csv",row.names = F)
-# write.csv(semdat.scaled.warm,"SEM/warm.csv",row.names = F )
-
-semdat.scaled.cool = read.csv("SEM/cool.csv")
-semdat.scaled.warm = read.csv("SEM/warm.csv")
-
-#initial model
-mod.8a <- 'func.struc ~ moisture + pH + temp + TN + TC + NH4N + NO3N + precipitation + GPP
-           func.richness ~ moisture + pH + temp + TN + TC + NH4N + NO3N + precipitation + GPP
-           bactr.struc ~ moisture + pH + temp + TN + TC + NH4N + NO3N + precipitation + GPP
-           bactr.richness ~ moisture + pH + temp + TN + TC + NH4N + NO3N + precipitation + GPP
-           copy_number ~ moisture + pH + temp + TN + TC + NH4N + NO3N + precipitation + GPP
-           GPP ~ moisture + pH + temp + TN + TC + NH4N + NO3N + precipitation
-           RS ~ func.struc + func.richness  + bactr.struc + bactr.richness + copy_number + GPP'
-fit.8a <- sem(mod.8a, data = semdat)
-summary(fit.8a, rsq=T, standardize = T)
-#The Std.lv column shows results that are standardized so that the latent variables 
-#have a variance of one. The Std.all column shows results that are standardized 
-#so that both the latent variables and the observed variables have a variance of one.
-#The latter is reported more often and is akin to standardized regression coefficients.
-fitMeasures(fit.8a)
-resid(fit.8a, type="standardized")
-modindices(fit.8a)
-mi1<-modindices(fit.8a);print(mi1[mi1$mi>4.0,])
-#Insignificant. P = 0.000
-
-#improved model temp + NH4N + precipitation + func.richness
-mod.8b <- 'func.struc ~ moisture + + NH4N + temp + TN + RA + copy_number + func.richness
-           func.richness ~ moisture + temp + RA + copy_number + TN + bactr.richness
-           bactr.struc ~ pH + TN + TC + copy_number
-           bactr.richness ~ pH + temp + TN + NO3N
-           copy_number ~ moisture + TC
-           GPP ~ moisture + TC + precipitation + RA
-           RS ~ func.struc + func.richness + GPP + TC + moisture + pH + NO3N'
-fit.8b <- sem(mod.8b, data=semdat)#bactr.richness + NEE + temp + precipitation + moisture
-fit.8b@SampleStats@cov
-summary(fit.8b, rsq=T, standardize = T)
-fitMeasures(fit.8b)
-
-resid(fit.8b, type="standardized")
-modindices(fit.8b)
-mi1<-modindices(fit.8b);print(mi1[mi1$mi>4.0,])
-
-anova(fit.8a,fit.8b)
-#p = 0.000
+library(psych)
+semdat.scaled.cool1 = semdat.scaled.cool[-8,]
+cor(semdat.scaled.cool1)
+pairs.panels(semdat.scaled.cool)
+pairs.panels(semdat.scaled.warm)
 
 
-# run diagnosis, remove insignificant links and add back suggested links
-
-sink("SEM/log_0123.txt")
-# Warm season SEM
+# hypothesized conceptual model
 mod.8a <- 'temp ~ warming
            moisture ~ warming
-           NO3N ~ warming + pH + temp
+           NO3N ~ warming
            GPP ~ warming + pH
-           pH ~ warming + temp
-           copy_number ~ temp + moisture + func.struc + GPP
-           func.struc ~ temp + moisture + NO3N
-           RS ~ func.struc + GPP + moisture + temp + NO3N
-           temp ~~ RS
-           temp ~~ NO3N'
+           pH ~ warming + NO3N
+           func.struc ~ moisture + NO3N + GPP + temp
+           bactr.richness ~ moisture + NO3N + GPP + temp + pH
+           copy_number ~ moisture + temp + NO3N + func.struc + bactr.richness + pHv
+           RH ~ moisture + temp + NO3N + func.struc + bactr.richness + copy_number
+           RS ~ moisture + temp + NO3N + func.struc + bactr.richness + copy_number + GPP
+'
 
-fit.8a <- sem(mod.8a, data=semdat.scaled.warm)
-summary(fit.8a, rsq=T, standardize = T,fit.measures=TRUE)
-fitMeasures(fit.8a)
-resid(fit.8a, type="cor")
-modindices(fit.8a)
-mi1<-modindices(fit.8a);print(mi1[mi1$mi>3.5,])
-
-#raw SEM plot
-library(semPlot)
-semPaths(fit.8a)
-semPaths(fit.8a,"std",residuals=FALSE,nCharNodes=0,layout="tree2",edge.label.cex = 1,sizeMan =12)
-
-# Cool season SEM
-mod.8d <- 'temp ~ warming
+# final model
+# Cool season ----
+mod.8a <- 'temp ~ warming
            moisture ~ warming
-           NO3N ~ warming + pH + temp
-           GPP ~ warming + pH
-           copy_number ~ temp + moisture + NO3N + func.struc
-           func.struc ~ temp + moisture + GPP
+           NO3N ~ warming
+           GPP ~ moisture + pH + warming
+           pH ~ warming + NO3N
+           func.struc ~ temp + moisture + NO3N + GPP
+           bactr.richness ~ pH + temp + moisture
+           copy_number ~ moisture + temp + NO3N + func.struc
+           RH ~ bactr.richness + temp + pH + copy_number 
            RS ~ func.struc + GPP + moisture + NO3N + temp
-           temp ~~ func.struc
-           temp ~~ NO3N
-           moisture ~~ GPP'
-#temp ~~ RS
-fit.8d <- sem(mod.8d, data=semdat.scaled.cool)
-summary(fit.8d, rsq=T, standardize = T,fit.measures=TRUE)
-fitMeasures(fit.8d)
-resid(fit.8d, type="cor")
-modindices(fit.8d)
-mi1<-modindices(fit.8d);print(mi1[mi1$mi>2,])
+           bactr.richness ~~ RS
+           func.struc ~~ bactr.richness
+'
 
-#raw SEM plot
-library(semPlot)
-semPaths(fit.8d)
-semPaths(fit.8d,"std",residuals=FALSE,nCharNodes=0,layout="tree2",
-         edge.label.cex = 1,sizeMan =12, nDigits = 3)
+fit.8a <- sem(mod.8a, data=semdat.scaled.cool1, estimator = "GLS")#bactr.richness + NEE + temp + precipitation + moisture
+semPaths(fit.8a, "std.all", edge.label.cex = 0.5, exoVar = FALSE, 
+         exoCov = FALSE)
+summary(fit.8a, rsq=T, standardize = T)
 
+fitMeasures(fit.8a)
 
+resid(fit.8a, type="standardized")
+modindices(fit.8a)
+mi1<-modindices(fit.8a);print(mi1[mi1$mi>1.0,])
+
+Beta <- lavInspect(fit.8a, "std.all")$beta # matrix of path coefficients
+# view effects of each order
+Beta # direct effects
+Beta %*% Beta # indirect effects involving 2 path coefficents
+Beta %*% Beta %*% Beta # indirect effects involving 3 path coefficients
+Beta %*% Beta %*% Beta %*% Beta # indirect effects involving 4 path coefficients
+Beta %*% Beta %*% Beta %*% Beta %*% Beta
+Beta %*% Beta %*% Beta %*% Beta %*% Beta %*% Beta
+
+toteff = as.data.frame(Beta + Beta %*% Beta + Beta %*% Beta %*% Beta +
+                         Beta %*% Beta %*% Beta %*% Beta + 
+                         Beta %*% Beta %*% Beta %*% Beta %*% Beta)
+RS_eff = toteff["RS",]
+RH_eff = toteff["RH",]
+write.csv(toteff, "SEM_RH/SEM_total_effects_cool_season.csv", row.names = T)
+
+#Warm season ----
+# initial
+mod.8a <- 'temp ~ warming
+           moisture ~ warming
+           NO3N ~ warming
+           GPP ~ warming + pH
+           pH ~ warming + NO3N
+           func.struc ~ moisture + NO3N + temp
+           bactr.richness ~ moisture + NO3N + temp + pH
+           copy_number ~ func.struc + temp
+           RH ~ moisture + func.struc
+           RS ~ moisture + temp + func.struc + GPP
+           NO3N ~~ bactr.richness
+'
+
+fit.8a <- sem(mod.8a, data=semdat.scaled.warm, estimator = "GLS")
+semPaths(fit.8a)
+summary(fit.8a, rsq=T, standardize = T)
+fitMeasures(fit.8a)
+
+resid(fit.8a, type="standardized")
+modindices(fit.8a)
+mi1<-modindices(fit.8a);print(mi1[mi1$mi>2.0,])
+
+Beta <- lavInspect(fit.8a, "std.all")$beta # matrix of path coefficients
+# view effects of each order
+Beta # direct effects
+Beta %*% Beta # indirect effects involving 2 path coefficents
+Beta %*% Beta %*% Beta # indirect effects involving 3 path coefficients
+Beta %*% Beta %*% Beta %*% Beta # indirect effects involving 4 path coefficients
+Beta %*% Beta %*% Beta %*% Beta %*% Beta
+Beta %*% Beta %*% Beta %*% Beta %*% Beta %*% Beta
+
+toteff = as.data.frame(Beta + Beta %*% Beta + Beta %*% Beta %*% Beta +
+                         Beta %*% Beta %*% Beta %*% Beta + 
+                         Beta %*% Beta %*% Beta %*% Beta %*% Beta)
+RS_eff = toteff["RS",]
+RH_eff = toteff["RH",]
+write.csv(toteff, "SEM_RH/SEM_total_effects_warm_season.csv", row.names = T)
+
+save.image("SEM_RH/SEM_revised_RHRS.RData")
+load("SEM_RH/SEM_revised_RHRS.RData")
